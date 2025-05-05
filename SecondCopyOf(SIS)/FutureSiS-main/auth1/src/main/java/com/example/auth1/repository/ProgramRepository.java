3d80878e9b7c664336de3cc57d66b32b5d8713c2
Mamiya <bolachrisjherico@gmail.com>
2025-05-05 13:47:15 +0800

    Delete SecondCopyOf(SIS)/FutureSiS-main/auth1 directory

--- SecondCopyOf(SIS)/FutureSiS-main/auth1/src/main/java/com/example/auth1/repository/ProgramRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.auth1.repository;
-
-import com.example.auth1.model.Program;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProgramRepository extends JpaRepository<Program, Long> {
-}
